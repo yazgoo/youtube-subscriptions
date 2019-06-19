a2c3e46346ede9be96957540f8f691d6bb1fbce1
yazgoo <yazgoo@gmail.com>
2019-06-19 10:41:53 +0200

    stuff

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,9 @@ extern crate sxd_document;
 extern crate sxd_xpath;
 extern crate dirs;
 extern crate reqwest;
+extern crate serde_json;
 
+use serde::{Serialize, Deserialize};
 use sxd_document::parser;
 use sxd_xpath::{evaluate_xpath, Value, Factory};
 use sxd_xpath::context::Context;
@@ -24,7 +26,7 @@ fn get_subscriptions_xml() -> Result<String, Error> {
     }
 }
 
-#[derive(Debug)]
+#[derive(Debug, Serialize, Deserialize, Clone)]
 struct Video {
     channel: String,
     title: String,
@@ -34,6 +36,10 @@ struct Video {
     description: String,
 }
 
+#[derive(Serialize, Deserialize, Debug)]
+struct Videos {
+    videos: Vec<Video>,
+}
 
 /*
 impl<'data, T: Sync + 'data> IntoParallelIterator for &'data [T] {
@@ -54,7 +60,7 @@ fn get_value(xpath: String, node: Element) -> String {
     return xpath.evaluate(&context, node).unwrap_or(Value::String("".to_string())).string().to_string();
 }
 
-fn get_videos(channel_url: String) -> Vec<Video> {
+fn get_channel_videos(channel_url: String) -> Vec<Video> {
     match reqwest::get(channel_url.as_str()) {
         Ok(mut result) => 
             match result.text() {
@@ -62,7 +68,6 @@ fn get_videos(channel_url: String) -> Vec<Video> {
                     let package = parser::parse(contents.as_str()).expect("failed to parse XML");
                     let document = package.as_document();
                     let title = evaluate_xpath(&document, "string(/*[local-name() = 'feed']/*[local-name() = 'title']/text())").unwrap_or(Value::String("".to_string())).string();
-                    println!("{:?}", channel_url);
                     match evaluate_xpath(&document, "/*[local-name() = 'feed']/*[local-name() = 'entry']") {
                         Ok(val) => {
                             if let Value::Nodeset(entries) = val {
@@ -79,64 +84,28 @@ fn get_videos(channel_url: String) -> Vec<Video> {
                                                  description: get_value("string(*[local-name() = 'group']/*[local-name() = 'description']/text())".to_string(), _element),
                                              }]
                                          },
-                                         None => vec![Video { 
-                                                 channel: "a".to_string(),
-                                                 title: "".to_string(),
-                                                 thumbnail: "".to_string(),
-                                                 url: "".to_string(),
-                                                 published: "".to_string(),
-                                                 description: "".to_string(),
-                                             }]
+                                         None => vec![]
                                          }
                                 ).collect()
                             }
                             else {
-                                vec![Video { 
-                                                 channel: "b".to_string(),
-                                                 title: "".to_string(),
-                                                 thumbnail: "".to_string(),
-                                                 url: "".to_string(),
-                                                 published: "".to_string(),
-                                                 description: "".to_string(),
-                                             }]
+                                vec![]
                             }
                         },
-                        Err(e) => {
-                            println!("{}", e);
-                            vec![Video { 
-                                                 channel: "c".to_string(),
-                                                 title: "".to_string(),
-                                                 thumbnail: "".to_string(),
-                                                 url: "".to_string(),
-                                                 published: "".to_string(),
-                                                 description: "".to_string(),
-                                             }]
+                        Err(_) => {
+                            vec![]
                         }
                     }
                 },
                 Err(_) =>
-                            vec![Video { 
-                                                 channel: "d".to_string(),
-                                                 title: "".to_string(),
-                                                 thumbnail: "".to_string(),
-                                                 url: "".to_string(),
-                                                 published: "".to_string(),
-                                                 description: "".to_string(),
-                                             }],
+                            vec![],
             },
         Err(_) =>
-                            vec![Video { 
-                                                 channel: "e".to_string(),
-                                                 title: "".to_string(),
-                                                 thumbnail: "".to_string(),
-                                                 url: "".to_string(),
-                                                 published: "".to_string(),
-                                                 description: "".to_string(),
-                                             }],
+                            vec![],
     }
 }
 
-fn get_channels(xml: String) -> Vec<Video> {
+fn get_videos(xml: String) -> Vec<Video> {
     let package = parser::parse(xml.as_str()).expect("failed to parse XML");
     let document = package.as_document();
     match evaluate_xpath(&document, "//outline/@xmlUrl") {
@@ -148,7 +117,7 @@ fn get_channels(xml: String) -> Vec<Video> {
                         None => None
                     }
                 ).flat_map( |url|
-                       get_videos(url)
+                       get_channel_videos(url)
                 ).collect()
             }
             else {
@@ -160,10 +129,29 @@ fn get_channels(xml: String) -> Vec<Video> {
     
 }
 
+fn to_show_videos(mut videos: Vec<Video>, start: usize, count: usize) -> Vec<Video> {
+    videos.sort_by(|a, b| b.published.cmp(&a.published));
+    return videos[start..count].to_vec();
+}
+
 fn main() {
     match get_subscriptions_xml() {
-        Ok(xml) =>
-            println!("{:?}", get_channels(xml)),
+        Ok(xml) => {
+            let path = "/tmp/yts.json";
+            if !fs::metadata(path).is_ok() {
+                let videos = Videos { videos: get_videos(xml)};
+                let serialized = serde_json::to_string(&videos).unwrap();
+                fs::write(path, serialized); 
+            }
+            match fs::read_to_string(path) {
+                Ok(s) => {
+                    let deserialized: Videos = serde_json::from_str(s.as_str()).unwrap();
+                    println!("{:?}", to_show_videos(deserialized.videos, 0, 2));
+                },
+                Err(e) =>
+                    println!("{}", e),
+            }
+        },
         Err(e) =>
             panic!("error parsing header: {:?}", e)
     }
