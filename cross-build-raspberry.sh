1abb73e2fe7e023e30e89d5639bbc37a1add56b1
yazgoo <yazgoo@gmail.com>
2019-06-30 15:52:10 +0200

    fix cross build script

--- cross-build-raspberry.sh
+++ cross-build-raspberry.sh
@@ -1,1 +1,5 @@
-docker run --volume "$PWD":/home/cross/project --volume "$PWD/db-deps:/home/cross/deb-deps" --volume "$HOME/.cargo/registry:/home/cross/.cargo/registry" ragnaroek/rust-raspberry:1.35.0 build --release
+mkdir -p "$PWD/.cargoregistry"
+docker run --volume "$PWD":/home/cross/project \
+  --volume "$PWD/db-deps:/home/cross/deb-deps" \
+  --volume "$PWD/.cargoregistry:/home/cross/.cargo/registry" \
+  ragnaroek/rust-raspberry:1.35.0 build --release
