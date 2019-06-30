aaca3c69ea7a09f16f64d817b331b88741ad8824
yazgoo <yazgoo@gmail.com>
2019-06-30 16:04:51 +0200

    fix cross build script

--- cross-build-raspberry.sh
+++ cross-build-raspberry.sh
@@ -1,5 +1,5 @@
 set -x
-[ -z "$TRAVIS_OS_NAME" ] || additional_argument="--volume "$HOME/.cargo/registry:/home/cross/.cargo/registry"
+[ -z "$TRAVIS_OS_NAME" ] || additional_argument="--volume $HOME/.cargo/registry:/home/cross/.cargo/registry"
 docker run --volume "$PWD":/home/cross/project \
   --volume "$PWD/db-deps:/home/cross/deb-deps" $additional_argument \
   ragnaroek/rust-raspberry:1.35.0 build --release
